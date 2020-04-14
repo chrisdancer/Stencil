e68315fa849330b84f02f41bd26f0b8ed51189db
Schmitt <WN00136865@wgs.wuerth.com>
2020-04-14 12:50:50 +0200

    add my-text component

---
+++ gestaltgesetze/src/components/my-text/my-text.tsx
@@ -0,0 +1,19 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'my-text',
+  styleUrl: 'my-text.css',
+ 
+})
+export class Text {
+  /**
+   * The first name
+   */
+  @Prop() content: string;
+
+  
+
+  render() {
+    return <p>{this.content}</p>;
+  }
+}
