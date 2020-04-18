de9b6f25e4d47beb37cb9257fb273655b2458221
chrisdancer <c.daenzer@otterbach.de>
2020-04-18 11:56:01 +0200

    just some testing for katha

--- gestaltgesetze/src/components/my-nav/my-nav.tsx
+++ gestaltgesetze/src/components/my-nav/my-nav.tsx
@@ -3,44 +3,63 @@ import { Component, h } from '@stencil/core';
 @Component({
   tag: 'my-nav',
   styleUrl: 'my-nav.css',
- 
+  shadow: true
 })
 
 export class MyNav {
 
 
 render() {
+  // var link="";
+  // if (window.location.href=="html") {
+  //   link ="";
+  // } else {
+  //   link ="..\\";
+  // }
+  // <li><a href= {link}"html">Start</a></li>
+  return <header>
+  <nav>
+    <ul class="navbar">
+      <li><a href="html">Start</a></li>
+      <li><a href="html\aehnlichkeit.html">Ähnlichkeit</a></li>
+      <li><a href="html\geschlossenheit.html">Geschlossenheit</a></li>
+      <li><a href="html\fortsetzung.html">Gute Fortsetzung</a></li>
+      <li><a href="html\naehe.html">Nähe</a></li>
+      <li><a href="html\praegnanz.html">Prägnanz</a></li>
+      <li><a href="html\schicksal.html">Schicksal</a></li>
+    </ul>
+  </nav>
+</header>;
+// if (window.location.href=="html"){
+//     return <header>
+//       <nav>
+//         <ul class="navbar">
+//           <li><a href="html">Start</a></li>
+//           <li><a href="html\aehnlichkeit.html">Ähnlichkeit</a></li>
+//           <li><a href="html\geschlossenheit.html">Geschlossenheit</a></li>
+//           <li><a href="html\fortsetzung.html">Gute Fortsetzung</a></li>
+//           <li><a href="html\naehe.html">Nähe</a></li>
+//           <li><a href="html\praegnanz.html">Prägnanz</a></li>
+//           <li><a href="html\schicksal.html">Schicksal</a></li>
+//         </ul>
+//       </nav>
+//     </header>;
 
-if (window.location.href=="html"){
-    return <header>
-      <nav>
-        <ul class="navbar">
-          <li><a href="html">Start</a></li>
-          <li><a href="html\aehnlichkeit.html">Ähnlichkeit</a></li>
-          <li><a href="html\geschlossenheit.html">Geschlossenheit</a></li>
-          <li><a href="html\fortsetzung.html">Gute Fortsetzung</a></li>
-          <li><a href="html\naehe.html">Nähe</a></li>
-          <li><a href="html\praegnanz.html">Prägnanz</a></li>
-          <li><a href="html\schicksal.html">Schicksal</a></li>
-        </ul>
-      </nav>
-    </header>;
+//     }else{
+//       return <header>
+//       <nav>
+//         <ul class="navbar">
+//           <li><a href="..\html">Start</a></li>
+//           <li><a href="..\html\aehnlichkeit.html">Ähnlichkeit</a></li>
+//           <li><a href="..\html\geschlossenheit.html">Geschlossenheit</a></li>
+//           <li><a href="..\html\fortsetzung.html">Gute Fortsetzung</a></li>
+//           <li><a href="..\html\naehe.html">Nähe</a></li>
+//           <li><a href="..\html\praegnanz.html">Prägnanz</a></li>
+//           <li><a href="..\html\schicksal.html">Schicksal</a></li>
+//         </ul>
+//       </nav>
+//     </header>;
 
-    }else{
-      return <header>
-      <nav>
-        <ul class="navbar">
-          <li><a href="..\html">Start</a></li>
-          <li><a href="..\html\aehnlichkeit.html">Ähnlichkeit</a></li>
-          <li><a href="..\html\geschlossenheit.html">Geschlossenheit</a></li>
-          <li><a href="..\html\fortsetzung.html">Gute Fortsetzung</a></li>
-          <li><a href="..\html\naehe.html">Nähe</a></li>
-          <li><a href="..\html\praegnanz.html">Prägnanz</a></li>
-          <li><a href="..\html\schicksal.html">Schicksal</a></li>
-        </ul>
-      </nav>
-    </header>;
-
-    }}
+    // }
+  }
 }
-
