7cc42ede465b902b5b0c9a9646acc35a91097e31
Schmitt <WN00136865@wgs.wuerth.com>
2020-05-04 15:11:21 +0200

    changed design template

--- gestaltgesetze/src/components/my-template/my-template.tsx
+++ gestaltgesetze/src/components/my-template/my-template.tsx
@@ -13,16 +13,16 @@ export class MyTemplate {
     return (
       <div>
         <my-nav></my-nav>
-        <my-headline></my-headline> 
-        <my-text content="zusammenfassung zu gesetz"></my-text>
-        <my-subheadline content="Beispiel 1"></my-subheadline>
-        <my-text content="erklärung zu bsp 1"></my-text>
-        <my-subheadline content="Beispiel 2"></my-subheadline>
-        <my-text content="erklärung zu bsp 2"></my-text>
-        <my-subheadline content="Beispiel 3"></my-subheadline>
-        <my-text content="erklärung zu bsp 3"></my-text>
-        <my-button></my-button>
-        <my-footer></my-footer>
+        <my-headline class="headline"></my-headline> 
+        <my-text class="defText" content="zusammenfassung zu gesetz"></my-text>
+        <my-subheadline class="sub"content="Beispiel 1"></my-subheadline>
+        <my-text class="text" content="erklärung zu bsp 1"></my-text>
+        <my-subheadline class="sub" content="Beispiel 2"></my-subheadline>
+        <my-text class="text" content="erklärung zu bsp 2"></my-text>
+        <my-subheadline class="sub" content="Beispiel 3"></my-subheadline>
+        <my-text class="text" content="erklärung zu bsp 3"></my-text>
+        <my-button class="button"></my-button>
+        <my-footer class="footer"></my-footer>
       </div>
     )
   }
