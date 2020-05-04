e7da70b798245e1b123d023cbe3cdcfabf63ebd7
Superscoti <57096064+Superscoti@users.noreply.github.com>
2020-05-04 14:07:49 +0200

    try

--- gestaltgesetze/src/components/my-flexdiv/my-flexdiv.tsx
+++ gestaltgesetze/src/components/my-flexdiv/my-flexdiv.tsx
@@ -9,6 +9,6 @@ import { Component, h } from '@stencil/core';
 export class MyFlexdiv {
 
   render() {
-    return <div id="flexdiv"></div>;
+    return <div id="flexdiv"><div class="item"></div><div class="item"></div><div class="item"></div></div>;
   }
 }
