37691e3fd7c6259f6dd79e6b30d68a24ed433521
Pascal Feinauer <pascalfeinauer@Pascals-MacBook-Pro.local>
2020-05-11 11:00:27 +0200

    added my definitiontext

---
+++ gestaltgesetze/src/components/my-definitiontext/my-definitiontext.tsx
@@ -0,0 +1,18 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'my-definitiontext',
+  styleUrl: 'my-definitiontext.css',
+  shadow: true
+})
+
+export class MyDefinitiontext {
+  
+  @Prop() content: string;
+
+  
+
+  render() {
+    return <div id="welcome">{this.content}</div>;
+  }
+}
