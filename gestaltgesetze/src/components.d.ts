99cc372f6263de649e98354befb61249673e1c57
Superscoti <57096064+Superscoti@users.noreply.github.com>
2020-05-11 11:43:16 +0200

    Update components.d.ts

--- gestaltgesetze/src/components.d.ts
+++ gestaltgesetze/src/components.d.ts
@@ -8,6 +8,9 @@ import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface MyButton {
     }
+    interface MyDefinitiontext {
+        "content": string;
+    }
     interface MyFlexdiv {
     }
     interface MyFooter {
@@ -44,6 +47,12 @@ declare global {
     var HTMLMyButtonElement: {
         prototype: HTMLMyButtonElement;
         new (): HTMLMyButtonElement;
+    };
+    interface HTMLMyDefinitiontextElement extends Components.MyDefinitiontext, HTMLStencilElement {
+    }
+    var HTMLMyDefinitiontextElement: {
+        prototype: HTMLMyDefinitiontextElement;
+        new (): HTMLMyDefinitiontextElement;
     };
     interface HTMLMyFlexdivElement extends Components.MyFlexdiv, HTMLStencilElement {
     }
@@ -113,6 +122,7 @@ declare global {
     };
     interface HTMLElementTagNameMap {
         "my-button": HTMLMyButtonElement;
+        "my-definitiontext": HTMLMyDefinitiontextElement;
         "my-flexdiv": HTMLMyFlexdivElement;
         "my-footer": HTMLMyFooterElement;
         "my-headline": HTMLMyHeadlineElement;
@@ -128,6 +138,9 @@ declare global {
 }
 declare namespace LocalJSX {
     interface MyButton {
+    }
+    interface MyDefinitiontext {
+        "content"?: string;
     }
     interface MyFlexdiv {
     }
@@ -160,6 +173,7 @@ declare namespace LocalJSX {
     }
     interface IntrinsicElements {
         "my-button": MyButton;
+        "my-definitiontext": MyDefinitiontext;
         "my-flexdiv": MyFlexdiv;
         "my-footer": MyFooter;
         "my-headline": MyHeadline;
@@ -178,6 +192,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "my-button": LocalJSX.MyButton & JSXBase.HTMLAttributes<HTMLMyButtonElement>;
+            "my-definitiontext": LocalJSX.MyDefinitiontext & JSXBase.HTMLAttributes<HTMLMyDefinitiontextElement>;
             "my-flexdiv": LocalJSX.MyFlexdiv & JSXBase.HTMLAttributes<HTMLMyFlexdivElement>;
             "my-footer": LocalJSX.MyFooter & JSXBase.HTMLAttributes<HTMLMyFooterElement>;
             "my-headline": LocalJSX.MyHeadline & JSXBase.HTMLAttributes<HTMLMyHeadlineElement>;
